8c414f6b3b54d452849c53114668f38759038beb
Przemyslaw Kaczor <przem.kacz7@gmai.com>
2019-02-12 18:27:38 +0100

    trzy.zmienne+tablica

---
+++ main.js
@@ -0,0 +1,8 @@
+var number1 = "4";
+console.log(number1);
+var number2 = "6";
+console.log(number2);
+var number3 = "7";
+console.log(number3);
+var table = [number1, number2,number3];
+document.getElementById("tablica").innerHTML = table;
